22071f2505c2f61e552d9dd51c55fb7b72ba465b
Piotr <bartnic1@gmail.com>
2018-04-12 13:38:34 +0000

    modified index

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 module.exports = {
-
-  function circ_circum(radius){
-    return 2*Math.pi*r;
-  }
+  circle_area, circle_circumference
 }
 
+function circle_area(radius){
+  return Math.pi*radius**2;
+}
+
+function circle_circumference(radius){
+  return 2*Math.pi*radius;
+}
